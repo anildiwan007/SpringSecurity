73804557498fdb2426ac748c76f211e20c6cc49e
anildiwan007 <anildiwan@gmail.com>
2015-08-01 22:54:28 -0500

    Changes for node, bower and gulp. Also added hello.js and other angular stuff

--- src/main/resources/static/js/hello.js
+++ src/main/resources/static/js/hello.js
@@ -1,3 +1,11 @@
 /**
  * 
  */
+(function () {
+	var angularApp = angular.module ("hello", []);
+	
+	var homeCtrl = angularApp.controller ("HomeCtrl", ['$scope', function ($scope) {
+		$scope.greeting.id = 1;
+		$scope.greeting.content = "Hello World";
+	}]);
+})();
